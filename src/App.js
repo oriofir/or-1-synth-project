5e024f35cfe4c27598d20df3933f808e0adad9da
oriofir <97814056+oriofir@users.noreply.github.com>
2022-04-08 11:38:34 -0700

    built out components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import React, { useState } from "react";
 import "./App.css";
 import { Song, Track, Instrument, Effect } from "reactronica";
-import { Button } from "bootstrap";
+import { Button } from "react-bootstrap";
 
 function App() {
   const [playing, setPlaying] = useState(false);
   return (
     <div>
       Working
-      <Song bpm={90} isPlaying={playing}>
+      <Song bpm={110} isPlaying={playing}>
         {" "}
         {/* Track with sequenced steps */}
-        <Track steps={["C3", null, "A3", "F3"]} volume={2}>
+        <Track steps={["C3", null, "A2", "F3"]} volume={2}>
           {/* Browser-based synth */}
           <Instrument type="monoSynth" />
           {/* Feedback effect  */}
           <Effect type="feedbackDelay" />
         </Track>
       </Song>
-      <button
+      <Button
         variant="primary"
         onClick={() => {
           setPlaying(!playing);
@@ -26,7 +26,7 @@ function App() {
       >
         {" "}
         {playing ? "Stop sound" : "Play sound"}
-      </button>
+      </Button>
     </div>
   );
 }
