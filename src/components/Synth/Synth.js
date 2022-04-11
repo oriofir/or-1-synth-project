ad7fe21e653312ae08d1eb9660f1a8557534d8e0
oriofir <97814056+oriofir@users.noreply.github.com>
2022-04-11 11:32:31 -0700

    removed the toggle button

--- src/components/Synth/Synth.js
+++ src/components/Synth/Synth.js
@@ -81,11 +81,6 @@ function Synth() {
           <label id={"Attack"}>Attack</label>
         </Donut>
       </Stack>
-      <DropdownButton id="dropdown-basic-button" title="Dropdown button">
-        <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-        <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-        <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
-      </DropdownButton>
     </div>
   );
 }
